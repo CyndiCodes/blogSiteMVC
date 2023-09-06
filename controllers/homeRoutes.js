81c14b4cd0006abcdb7c5c25cbfcb13c05259ed5
CyndiCodes <huertascyn@gmail.com>
2023-09-06 09:40:52 -0400

    added route for homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,30 @@ const router = require('express').Router();
 const { User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const commentData = await Comment.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const comments = commentData.map((project) => comment.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      comments, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
